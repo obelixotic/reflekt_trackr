8972ce91a072b148dfad9bd0a707c1f70d7a8b07
asd0999 <obelixotic@gmail.com>
2020-10-30 11:56:58 -0400

    first commit, basic server set up

---
+++ server.js
@@ -0,0 +1,46 @@
+// DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+
+// CONFIGURATIONS
+require('dotenv').config();
+const post = process.env.PORT;
+const mongodbURI = process.env.MONGODBURI;
+const app = express();
+mongoose.connection();
+
+// DATABASE
+mongoose.connect(
+    mongodbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    },
+    () => {
+        console.log('the connection with mongod is established at', mongodbURI)
+    }
+);
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(morgan(':method :url :status'));
+app.set('view engine', 'ejs');
+app.use(
+    session({
+        secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+        resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+        saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+);
+
+// ROUTES
+
+// LISTENER
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
