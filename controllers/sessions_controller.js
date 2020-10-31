39f3f5e2204b147fa51260334640de1cd1dc6b65
asd0999 <obelixotic@gmail.com>
2020-10-30 21:35:18 -0400

    log in page basic functioning working

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const User = require('../models/users_model');
 const sessions = express.Router();
 
 // ROUTES
 // new
 sessions.get('/new', (req, res) => {
-    res.send('log in page');
+    // res.send('log in page');
+    res.render('sessions/new.ejs');
 });
 
 // create
 sessions.post('/', (req, res) => {
-    res.send(`if valid, user signed in with creds: ${req.body.username}, ${req.body.password}`);
+    // res.send(`if valid, user signed in with creds: ${req.body.username}, ${req.body.password}`);
+    console.log(`${req.body.username}, ${req.body.password}`);
+    User.findOne({ username: req.body.username }, (err, foundUser) => {
+        console.log(foundUser);
+        if (foundUser) {
+            req.session.curentUser = foundUser;
+            res.send(req.session.curentUser);
+        } else {
+            // req.session.curentUser = 'none';
+            res.redirect('/sessions/new');
+        }
+    });
 });
 
 module.exports = sessions;
