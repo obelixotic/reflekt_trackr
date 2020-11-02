9da0dd2fa2b8f6a3dd33fa587c747e81a5b57b99
asd0999 <obelixotic@gmail.com>
2020-11-02 17:49:49 -0500

    basic CSS

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan(':method :url :status'));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(
     session({
