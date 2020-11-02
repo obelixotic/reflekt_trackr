071faca777c55a116e258b12c8295c650c14a4b2
asd0999 <obelixotic@gmail.com>
2020-11-02 12:11:40 -0500

    updated user schema --> dont need habits array

--- models/users_model.js
+++ models/users_model.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    habits: [String]
+    password: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userSchema);
