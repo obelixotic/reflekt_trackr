394479da263d23e1db0ac3265b861b68475844a0
asd0999 <obelixotic@gmail.com>
2021-01-16 13:04:07 -0500

    mongodbURI

--- server.js
+++ server.js
@@ -1,60 +1,61 @@
 // MASTER BRANCH FOR NEW FEATURES/EDITS
 
 // DEPENDENCIES
-const express = require('express');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
 
 // CONFIGURATIONS
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT;
 const mongodbURI = process.env.MONGODBURI;
 const app = express();
 mongoose.connection;
+//MONGODBURI=mongodb://localhost:27017/reflekt_db
 
 // DATABASE
 mongoose.connect(
     mongodbURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false
+        useFindAndModify: false,
     },
     () => {
-        console.log('the connection with mongod is established at', mongodbURI)
+        console.log("the connection with mongod is established at", mongodbURI);
     }
 );
 
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(morgan(':method :url :status'));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+app.use(morgan(":method :url :status"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(
     session({
         secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
         resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
-        saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+        saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
     })
 );
 
 // CONTROLLERS
-const habitsController = require('./controllers/habits_controller.js');
-app.use('/habits', habitsController);
+const habitsController = require("./controllers/habits_controller.js");
+app.use("/habits", habitsController);
 
-const usersController = require('./controllers/users_controller.js');
-app.use('/users', usersController);
+const usersController = require("./controllers/users_controller.js");
+app.use("/users", usersController);
 
-const sessionsController = require('./controllers/sessions_controller.js');
-app.use('/sessions', sessionsController);
+const sessionsController = require("./controllers/sessions_controller.js");
+app.use("/sessions", sessionsController);
 
 // ROUTES
 // index
-app.get('/', (req, res) => {
-    res.redirect('/habits');
+app.get("/", (req, res) => {
+    res.redirect("/habits");
 });
 
 // LISTENER
